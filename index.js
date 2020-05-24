b019f30a4de1952ee19233ff6efbf2b426094287
Stephanie Vazquez (Oculus) <thestephanie@fb.com>
2020-05-23 23:22:30 -0700

    Added JS code for to-do list.

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const list = document.querySelector('.list');
+const input = document.querySelector('.enter-item-input');
+const button = document.querySelector('.add-item-button');
+
+button.onclick = function() {
+  let myItem = input.value;
+  input.value = '';
+
+  const listItem = document.createElement('li');
+  const listText = document.createElement('span');
+  const listBtn = document.createElement('button');
+
+  listItem.appendChild(listText);
+  listText.textContent = myItem;
+  listItem.appendChild(listBtn);
+  listBtn.textContent = 'Delete';
+  list.appendChild(listItem);
+
+  listBtn.onclick = function(e) {
+    list.removeChild(listItem);
+  }
+
+  input.focus();
+}
