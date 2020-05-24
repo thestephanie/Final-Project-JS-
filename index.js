e9afbd060aaa345b58d5b4fa79239e873a480374
Stephanie Vazquez (Oculus) <thestephanie@fb.com>
2020-05-24 12:27:11 -0700

    JS code for timer.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+// Code below is for to-do list. 
 const list = document.querySelector('.list');
 const input = document.querySelector('.enter-item-input');
 const button = document.querySelector('.add-item-button');
@@ -22,4 +22,38 @@ button.onclick = function() {
   }
 
   input.focus();
+}
+
+// Code below is for timer. 
+const timerText = document.querySelector('.timer-text')
+const startButton = document.querySelector('.start-button')
+
+let isTimerRunning = false;
+let interval = null;
+
+startButton.onclick = function() {
+  let timeLeft = 25 * 60;
+  if (isTimerRunning === false) {
+    startButton.innerText = 'Stop'
+    isTimerRunning = true;
+    // Time in seconds
+    function updateTimer() {
+      if (timeLeft > 0) {
+        timeLeft--;
+        const minutes = Math.floor(timeLeft / 60);
+        const seconds = timeLeft - minutes * 60;
+        const displayTime = minutes + ':' + seconds;
+        timerText.innerText = displayTime;
+      } else if (interval !== null) {
+        clearInterval(interval)
+        startButton.innerText = 'Start'
+      }
+    }
+    interval = setInterval(updateTimer, 1000);
+  } else {
+    if (interval !== null) {
+      clearInterval(interval);
+      startButton.innerText = 'Start'
+    }
+  }
 }
